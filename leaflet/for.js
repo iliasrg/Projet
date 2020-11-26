5817da88eb9ebf8c393dac9baa6ff9c6eb8db995
iliasrg <58733877+iliasrg@users.noreply.github.com>
2020-11-26 11:45:28 +0100

    Add files via upload

---
+++ leaflet/for.js
@@ -0,0 +1,12 @@
+var pnt = {
+"type": "FeatureCollection",
+"name": "Forest_fires",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "AREA": 14.963, "PERIMETER": 25.082, "CNTCODE": 222, "NAME": "TUNISIA", "Nbincendi": 26, "Hetares": 3210 }, "geometry": { "type": "Point", "coordinates": [ 9.576580190322979, 34.114157366976997 ] } },
+{ "type": "Feature", "properties": { "AREA": 213.238, "PERIMETER": 71.979, "CNTCODE": 4, "NAME": "ALGERIA", "Nbincendi": 164, "Hetares": 48512 }, "geometry": { "type": "Point", "coordinates": [ 2.671715109879909, 28.181055621230975 ] } },
+{ "type": "Feature", "properties": { "AREA": 147.241, "PERIMETER": 58.016, "CNTCODE": 124, "NAME": "LIBYA", "Nbincendi": 8, "Hetares": 716 }, "geometry": { "type": "Point", "coordinates": [ 18.034157201270865, 27.048962383017162 ] } },
+{ "type": "Feature", "properties": { "AREA": 90.207, "PERIMETER": 56.28, "CNTCODE": 59, "NAME": "EGYPT", "Nbincendi": 257, "Hetares": 30678 }, "geometry": { "type": "Point", "coordinates": [ 29.861944388996132, 26.487878680574788 ] } },
+{ "type": "Feature", "properties": { "AREA": 38.767, "PERIMETER": 38.082, "CNTCODE": 143, "NAME": "MOROCCO", "Nbincendi": 26, "Hetares": 4811 }, "geometry": { "type": "Point", "coordinates": [ -8.924855937174195, 29.103399889094788 ] } }
+]
+}
